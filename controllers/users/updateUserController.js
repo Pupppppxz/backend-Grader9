02193655913c152d2b0cb78ac732eaf52f8821b3
Pupppppxz <63011078@kmitl.ac.th>
2021-04-26 02:56:03 +0700

    return data from update

--- controllers/users/updateUserController.js
+++ controllers/users/updateUserController.js
@@ -1,6 +1,6 @@
 const { updateUserService } = require('../../services/users')
 
 module.exports = async function updateUserController(req, res) {
-    await updateUserService(req.params.id, req.body)
-    return res.sendStatus(200)
+    const update = await updateUserService(req.params.id, req.body)
+    return res.send(update)
 }
