9e423391444c23441d3eb69cfc89a82301ef6e97
Pupppppxz <69138763+Pupppppxz@users.noreply.github.com>
2021-05-21 10:29:16 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -26,6 +26,10 @@ const UserSchema = new mongoose.Schema({
     type: Number,
     default: 0
   },
+  totalSubmit: {
+    type: Number,
+    default: 0
+  },
   date: {
     type: Date,
     default: Date.now
