496f58661e4a2de8aba3fcb99eaafc15d5e77576
Pupppppxz <63011078@kmitl.ac.th>
2021-06-14 23:25:25 +0700

    update mongoose.connect

--- config/key.js
+++ config/key.js
@@ -1,10 +1,4 @@
 module.exports = {
     mongoURI: "mongodb://localhost:27017/Boostup9_2",
     secretOrKey: "secret",
-    GRADER_URL: '',
-    GRADER_USER: "CE",
-    MONGO_PASS: "1=f!|TI58oN_H4zjKmJd*MmTAtgq",
 }
-
-//mongodb+srv://admin:admin@cluster0.q0rvj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-// mongodb://localhost:27017/Boostup9_2
--- server.js
+++ server.js
@@ -3,34 +3,42 @@ const mongoose = require("mongoose")
 const passport = require("passport")
 const routes = require('./routes')
 const cors = require('cors')
-const { mongoURI, GRADER_USER } = require('./config/key')
+const { mongoURI } = require('./config/key')
 const app = express()
+const dotenv = require('dotenv')
+dotenv.config()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use('/uploads', express.static('uploads'))
 
-const db = require('./config/key').mongoURI
-mongoose.connect(db,
-{ 
+// const db = require('./config/key').mongoURI
+// mongoose.connect(db,
+// { 
+//     useNewUrlParser: true, 
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+// })
+// .then(() => console.log('Success connect'))
+// .catch(err => console.log(err))
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.MONGO_URL,{
+    "auth": {"authSource":"admin"},
+    "user": String(process.env.MONGO_USER),
+    "pass": process.env.MONGO_PASS,
     useNewUrlParser: true, 
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
-})
-.then(() => console.log('Success connect'))
-.catch(err => console.log(err))
-// mongoose.Promise = global.Promise
-// mongoose.connect(`${mongoURI}`,{
-//     "auth": {"authSource":"admin"},
-//     "user": GRADER_USER,
-//     "pass":
-// })
+}).then(() => {
+    console.log(`Mongo connected to database`)
+}).catch(err => console.log(err))
 
 require('./config/passport')(passport)
 app.use(passport.initialize())
 
 app.use('/api', routes)
-const port = 5000;
-app.listen(process.env.PORT || port, () => console.log(`Server start at port ${port}`))
+port = 5000
+app.listen(process.env.PORT || 5000, () => console.log(`Server start at port ${port}`))
