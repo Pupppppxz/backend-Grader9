223f83db4fcdcb5d599bb35d4d6d98d99741e8a9
Pupppppxz <63011078@kmitl.ac.th>
2021-06-18 01:02:33 +0700

    gg

--- services/questions/getQuestionService.js
+++ services/questions/getQuestionService.js
@@ -12,39 +12,22 @@ const getSubmit = async function(id){
                         .find({userId: id})
                         .select(['status','questionId','result'])
                         .sort({number: 'asc'})
-    return question
+    if(question.length === 0){
+        return 0
+    } else {
+        return question
+    }
 }
 
 module.exports = async function getQuestionService(userId){
     const question = await getQuestion()
-    console.log(question);
+    // console.log(question);
     const submit = await getSubmit(userId)
     console.log(submit);
     let item = []
     let count = 0
     for(i = 0; i < question.length; i++) {
-        if(submit[count].questionId === question[i]._id.toString()) {
-            let items = {
-                _id: question[i]._id,
-                status: submit[count].status,
-                question: question[i].question,
-                rank: question[i].rank,
-                chaya: question[i].chaya,
-                unit: question[i].unit,
-                detail: question[i].detail,
-                str_input_1: question[i].str_input_1,
-                str_output_1: question[i].str_output_2,
-                str_input_2: question[i].str_input_2,
-                str_output_2: question[i].str_output_2,
-                str_input_3: question[i].str_input_3,
-                str_output_3: question[i].str_output_2,
-                number: question[i].number,
-                result: submit[count].result,
-                finished: question[i].finished
-            }
-            item.push(items)
-            count = count + 1
-        } else {
+        if(submit === 0){
             let items = {
                 _id: question[i]._id,
                 status: question[i].status,
@@ -64,6 +47,49 @@ module.exports = async function getQuestionService(userId){
                 finished: question[i].finished
             }
             item.push(items)
+        } else {
+            if(submit[count].questionId === question[i]._id.toString()) {
+                let items = {
+                    _id: question[i]._id,
+                    status: submit[count].status,
+                    question: question[i].question,
+                    rank: question[i].rank,
+                    chaya: question[i].chaya,
+                    unit: question[i].unit,
+                    detail: question[i].detail,
+                    str_input_1: question[i].str_input_1,
+                    str_output_1: question[i].str_output_2,
+                    str_input_2: question[i].str_input_2,
+                    str_output_2: question[i].str_output_2,
+                    str_input_3: question[i].str_input_3,
+                    str_output_3: question[i].str_output_2,
+                    number: question[i].number,
+                    result: submit[count].result,
+                    finished: question[i].finished
+                }
+                item.push(items)
+                count = count + 1
+            } else {
+                let items = {
+                    _id: question[i]._id,
+                    status: question[i].status,
+                    question: question[i].question,
+                    rank: question[i].rank,
+                    chaya: question[i].chaya,
+                    unit: question[i].unit,
+                    detail: question[i].detail,
+                    str_input_1: question[i].str_input_1,
+                    str_output_1: question[i].str_output_2,
+                    str_input_2: question[i].str_input_2,
+                    str_output_2: question[i].str_output_2,
+                    str_input_3: question[i].str_input_3,
+                    str_output_3: question[i].str_output_2,
+                    number: question[i].number,
+                    result: "----",
+                    finished: question[i].finished
+                }
+                item.push(items)
+            } 
         }
     }
     return item;
