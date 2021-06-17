7a697a48dcae4e79d49f771952a4829bc3e98cc1
Pupppppxz <69138763+Pupppppxz@users.noreply.github.com>
2021-06-17 16:26:19 +0700

    Update addSuccessSubmissionService.js

--- services/submissions/addSuccessSubmissionService.js
+++ services/submissions/addSuccessSubmissionService.js
@@ -1,6 +1,6 @@
 const { QuestionModel } = require("../../models")
 
-module.exports = async function addSuccessSubmissionService(qId) {
+module.exports = async function addSuccessSubmissionService(qId, score) {
     const { finished } = await QuestionModel.findOne({_id: qId})
-    return QuestionModel.findOneAndUpdate({_id: qId}, {finished: Number(finished + 1)})
+    return QuestionModel.findOneAndUpdate({_id: qId}, {finished: Number(finished) + Number(score)})
 }
