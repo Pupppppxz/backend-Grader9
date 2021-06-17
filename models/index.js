e22d5ee36cacb38e039da9d8a4e18823e2e01ebe
Pupppppxz <69138763+Pupppppxz@users.noreply.github.com>
2021-06-17 16:24:18 +0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -2,12 +2,10 @@ const UserModel = require('./User')
 const QuestionModel = require('./Question')
 const SubmitModel = require('./Submit')
 const SubmitCodeModel = require('./submitCode')
-const ActivityModel = require('./activity')
 
 module.exports = {
     UserModel,
     QuestionModel,
     SubmitModel,
-    SubmitCodeModel,
-    ActivityModel
+    SubmitCodeModel
 }
