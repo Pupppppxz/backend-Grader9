8dd794baa161754ff1910f5f9e96192a03fca220
Pupppppxz <63011078@kmitl.ac.th>
2021-06-30 18:40:06 +0700

    kk

--- controllers/submissions/fetchSubmissionController.js
+++ controllers/submissions/fetchSubmissionController.js
@@ -1,6 +1,6 @@
 const { fetchSubmissionService } = require("../../services/submissions")
 
 module.exports = async function fetchSubmissionController(req, res) {
-    const fetch = await fetchSubmissionService(req.body)
-    return res.send(fetch).status(200)
+    await fetchSubmissionService(req.body)
+    return res.status(200)
 }
