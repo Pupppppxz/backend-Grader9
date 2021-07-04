90dcd7bf35dd324fa6dce672eee9cb0b2a02de43
Pupppppxz <69138763+Pupppppxz@users.noreply.github.com>
2021-07-05 03:49:28 +0700

    Update deleteQuestionService.js

--- services/questions/deleteQuestionService.js
+++ services/questions/deleteQuestionService.js
@@ -1,5 +1,16 @@
 const { QuestionModel, SubmitModel, SubmitCodeModel } = require('../../models')
+const { updateUserScoreService } = require('../submissions')
+const { getUserScoreService } = require('../users')
 const mongoose = require('mongoose')
+
+const getSubmit = async function(id) {
+    const submitArr = await SubmitModel.find({questionId: id})
+    if(submitArr.length === 0) {
+        return 0
+    } else {
+        return submitArr   
+    }
+}
 
 const deleteQuestion = async function(id){
     const deleteQuestion = await QuestionModel.findByIdAndDelete(id)
@@ -7,7 +18,7 @@ const deleteQuestion = async function(id){
 }
 
 const deleteSubmit = async function(id) {
-    const deleteSubmit = await SubmitModel.deleteMany({questionId: id})
+    const deleteSubmit = await SubmitModel.findOneAndDelete({questionId: id})
     return deleteSubmit
 }
 
@@ -17,8 +28,24 @@ const deleteSubmitCode = async function(id) {
 }
 
 module.exports = async function deleteQuestionService(id) {
-    await deleteQuestion(id)
-    await deleteSubmit(id)
-    await deleteSubmitCode(id)
+    try {
+        const submit = await getSubmit(id)
+        if(submit === 0) {
+            await deleteQuestion(id)
+        } else {
+            for(let i = 0; i < submit.length; i++){
+                const oldScore = await getUserScoreService(submit[i].userId)
+                if(submit[i].score !== 0){
+                   await updateUserScoreService(submit[i].userId, submit[i].score, oldScore, "minus")    
+                }
+                await deleteSubmit(submit[i]._id)
+            }
+        }      
+    } catch (err) {
+        console.log(err)   
+    } finally {
+        await deleteSubmitCode(id)
+        await deleteQuestion(id)
+    }
     return {Success: "deleted!"}
 }
