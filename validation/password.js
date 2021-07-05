6747c005613959b6c98831c250efd9c0e94ccabd
Pupppppxz <69138763+Pupppppxz@users.noreply.github.com>
2021-07-05 15:58:33 +0700

    Update password.js

--- validation/password.js
+++ validation/password.js
@@ -4,8 +4,13 @@ const isEmpty = require('is-empty')
 module.exports = function validatePassword(data) {
     let err = {}
 
+    data.oldPassword = !isEmpty(data.oldPassword) ? data.oldPassword : ""
     data.password = !isEmpty(data.password) ? data.password : ""
     data.password2 = !isEmpty(data.password2) ? data.password2 : ""
+
+    if(Validator.isEmpty(data.oldPassword)) {
+        err.oldPassword = "Old password field is required"
+    }
 
     if(Validator.isEmpty(data.password)) {
         err.password = "Password field is required"
@@ -17,6 +22,10 @@ module.exports = function validatePassword(data) {
 
     if(!Validator.isLength(data.password, {min: 8, max: 30})){
         err.password = "Password must be at least 6 charactor"
+    }
+
+    if(!Validator.equals(data.password, data.password2)){
+        err.password2 = "Password must match"
     }
 
     return {
