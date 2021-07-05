dcdde8e50840b40eccf592642c1e2b07f87c850d
Pupppppxz <63011078@kmitl.ac.th>
2021-07-05 19:00:59 +0700

    LL

--- controllers/submissions/fetchSubmissionController.js
+++ controllers/submissions/fetchSubmissionController.js
@@ -1,10 +1,6 @@
 const { fetchSubmissionService } = require("../../services/submissions")
 
 module.exports = async function fetchSubmissionController(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-    res.setHeader('Access-Control-Allow-Credentials', true)
     const data = await fetchSubmissionService(req.body)
     return res.status(200).send(data)
 }
