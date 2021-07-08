2242a94630a67e7c181561c60e3537dc63c514fa
Pupppppxz <63011078@kmitl.ac.th>
2021-07-08 21:03:44 +0700

    KK

--- controllers/questions/getQuestionController.js
+++ controllers/questions/getQuestionController.js
@@ -1,6 +1,10 @@
 const { getQuestionService } = require('../../services/questions')
 
 module.exports = async function getQuestionController(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+    res.setHeader('Access-Control-Allow-Credentials', true)
     const question = await getQuestionService(req.query.id)
     return res.send(question)
 }
