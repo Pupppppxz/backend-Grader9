598e909f2e707570ece48eb056e8632b1fa6caf0
Pupppppxz <63011078@kmitl.ac.th>
2021-07-09 22:54:13 +0700

    KK

--- services/submissions/getFinishSubmissionCodeService.js
+++ services/submissions/getFinishSubmissionCodeService.js
@@ -1,6 +1,17 @@
 const { SubmitCodeModel } = require('../../models')
+const moment = require('moment')
 
 module.exports = async function getFinishSubmissionCodeService(qId) {
-    const code = await SubmitCodeModel.find({questionId: qId, status: 2}).select(['code']).limit(5)
-    return code
+    const code = await SubmitCodeModel.find({questionId: qId, status: 2}).select(['code','createdAt']).limit(5)
+    let items = []
+    for(i = 0; i < code.length; i++) {
+        let time = moment(code[i].createdAt)
+        let item = {
+            code: code[i].code,
+            status: code[i].status,
+            time: time.utcOffset('+0700').format('l') + " " + time.utcOffset('+0700').format('LTS')
+        }
+        items.push(item)
+    }
+    return items
 }
