43ba76fe07a0a911d6c1162d66e29bd0484e2a11
Pupppppxz <63011078@kmitl.ac.th>
2021-07-10 00:48:35 +0700

    update score board

--- services/users/getScoreBoardService.js
+++ services/users/getScoreBoardService.js
@@ -5,6 +5,6 @@ module.exports = async function getScoreBoardService(status) {
     .find({userStatus: status})
     .select(['_id','score','nickName','username','finished','profilePicture','group','commit'])
     .sort({score: 'desc'})
-    const board = scoreBoard.filter(user => user.group !== 5)
+    const board = scoreBoard.filter(user => user.group < 5)
     return board
 }
