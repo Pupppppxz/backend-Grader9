7b18264e81e767b1b7e96c9633bf01ca0dc29d97
Pupppppxz <63011078@kmitl.ac.th>
2021-07-13 02:45:27 +0700

    KK

--- controllers/admin/autoLogoutController.js
+++ controllers/admin/autoLogoutController.js
@@ -1,6 +1,10 @@
 const { setUpdateAll } = require('../../middleware/updated')
 
 module.exports = async function autoLogoutController(req, res) {
-    await setUpdateAll()
-    return res.status(200).json({updateSuccess: "Success!"})
+    if(req.headers.logoutUser === process.env.CONFIRM) {
+        await setUpdateAll()
+        return res.status(200).json({updateSuccess: "Success!"})
+    } else {
+        return res.status(400).json({again: "try again!"})
+    }
 }
