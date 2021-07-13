f4fa3d47e5175fa74fa20020ac89b8bc31889dbc
Pupppppxz <63011078@kmitl.ac.th>
2021-07-13 22:49:13 +0700

    update question-add

--- services/questions/addQuestionService.js
+++ services/questions/addQuestionService.js
@@ -2,6 +2,5 @@ const { QuestionModel } = require('../../models')
 
 module.exports = async function addQuestionService(data) {
     const question = new QuestionModel(data)
-    await question.save()
-    return question._id
+    return question.save()
 }
