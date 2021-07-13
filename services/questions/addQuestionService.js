9f34c4f6b49be30487350a64f45ee4d7391d611a
Pupppppxz <63011078@kmitl.ac.th>
2021-07-13 22:42:10 +0700

    KK

--- services/questions/addQuestionService.js
+++ services/questions/addQuestionService.js
@@ -2,6 +2,6 @@ const { QuestionModel } = require('../../models')
 
 module.exports = async function addQuestionService(data) {
     const question = new QuestionModel(data)
-    question.save()
+    await question.save()
     return question._id
 }
