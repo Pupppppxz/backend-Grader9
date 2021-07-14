bcd47f3fc8e6c3725a159a825966cb4979167472
Pupppppxz <63011078@kmitl.ac.th>
2021-07-14 19:52:56 +0700

    KK

--- services/submissions/getFinishSubmissionCodeService.js
+++ services/submissions/getFinishSubmissionCodeService.js
@@ -6,12 +6,13 @@ const checkUser = async (id) => {
 }
 
 module.exports = async function getFinishSubmissionCodeService(qId) {
-    const code = await SubmitCodeModel.find({questionId: qId, status: 2}).select(['code','createdAt','userId','status']).limit(5)
+    const code = await SubmitCodeModel.find({questionId: qId, status: 2}).select(['code','createdAt','userId','status'])
     let items = []
+    let count = 0;
     for(i = 0; i < code.length; i++) {
         const { group } = await checkUser(code[i].userId)
         if(group) {
-            if(group < 5) {
+            if(group < 5 && count < 5) {
                 let time = moment(code[i].createdAt)
                 let item = {
                     code: code[i].code,
@@ -19,6 +20,7 @@ module.exports = async function getFinishSubmissionCodeService(qId) {
                     time: time.utcOffset('+0700').format('l') + " " + time.utcOffset('+0700').format('LTS')
                 }
                 items.push(item)
+                count = count + 1
             }
         }
     }
