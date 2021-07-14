9f60732850e4e4c58e4b16becda7569141e0751d
Pupppppxz <69138763+Pupppppxz@users.noreply.github.com>
2021-07-14 17:21:02 +0700

    Update loginService.js

--- services/users/loginService.js
+++ services/users/loginService.js
@@ -7,6 +7,10 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 module.exports = async function loginUser(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+    res.setHeader('Access-Control-Allow-Credentials', true)
     try {
         const username = req.body.username
         const password = req.body.password
