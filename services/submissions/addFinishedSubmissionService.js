77efd7ebb65d36cfecad80d5175ef1f36ab622d8
Pupppppxz <63011078@kmitl.ac.th>
2021-07-22 22:39:35 +0700

    KK

--- services/submissions/addFinishedSubmissionService.js
+++ services/submissions/addFinishedSubmissionService.js
@@ -2,7 +2,7 @@ const {UserModel} = require('../../models')
 
 module.exports = async function addFinishedSubmissionService(userId, type) {
     const user = await UserModel.findOne({_id: userId})
-    if(type === "minus") {
+    if(type == "minus") {
         return UserModel.findOneAndUpdate({_id: userId}, {finished: user.finished - 1})
     } else {
         return UserModel.findOneAndUpdate({_id: userId}, {finished: user.finished + 1})
--- services/submissions/addSuccessSubmissionService.js
+++ services/submissions/addSuccessSubmissionService.js
@@ -2,7 +2,7 @@ const { QuestionModel } = require("../../models")
 
 module.exports = async function addSuccessSubmissionService(qId, type) {
     const question = await QuestionModel.findOne({_id: qId})
-    if(type === "minus") {
+    if(type == "minus") {
         return QuestionModel.findOneAndUpdate({_id: qId}, {finished: question.finished - 1})
     } else {
         return QuestionModel.findOneAndUpdate({_id: qId}, {finished: question.finished + 1})
